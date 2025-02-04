541411eef3cb1e6977d5dc17d418bc72142e469d
OmerAtayilmaz <atay.omer33@gmail.com>
2025-02-04 15:30:43 +0300

    Cors and rate limiting implemented

--- routes/api.php
+++ routes/api.php
@@ -5,15 +5,20 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\ContactController;
 use App\Http\Controllers\Api\V1\QuoteController;
 
-Route::prefix('v1')->group(function(){
+Route::middleware('throttle:api')->group(
+    function(){
+        Route::prefix('v1')->group(function(){
 
-    //Contact Form Applies
-    Route::post('contact', [ContactController::class, 'store']);
+            //Contact Form Applies
+            Route::post('contact', [ContactController::class, 'store']);
+        
+            // Quote Form Applies
+            Route::post('quote', [QuoteController::class, 'store']);
+        });
+        
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        })->middleware('auth:sanctum');
+    }
+);
 
-    // Quote Form Applies
-    Route::post('quote', [QuoteController::class, 'store']);
-});
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
