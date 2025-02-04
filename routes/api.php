322d037edfda0fd55b269c9bb369a4825c4e181b
OmerAtayilmaz <atay.omer33@gmail.com>
2025-02-04 13:51:58 +0300

    filament init

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\ContactController;
+use App\Http\Controllers\Api\V1\QuoteController;
+
+Route::prefix('v1')->group(function(){
+
+    //Contact Form Applies
+    Route::post('contact', [ContactController::class, 'store']);
+
+    // Quote Form Applies
+    Route::post('quote', [QuoteController::class, 'store']);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
