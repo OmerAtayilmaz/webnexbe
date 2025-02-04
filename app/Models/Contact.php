ecfcf44d20422c9a97fb95e63ff7c7e0741deb24
OmerAtayilmaz <atay.omer33@gmail.com>
2025-02-04 14:29:13 +0300

    Contact filament structure and POST request completed

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    //
+    //fillable 
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'message',
+    ];
+
+ 
 }
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,10 +25,11 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('backoffice')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Purple,
+                'dark' => Color::Gray,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
